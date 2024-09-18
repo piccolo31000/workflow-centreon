6e71f95749dd137c149ffa0fd182234d01a25b5c
s chapron <116343705+piccolo31000@users.noreply.github.com>
2024-09-18 16:10:28 +0200

    Update conf.php

--- centreon-dsm/www/modules/centreon-dsm/conf.php
+++ centreon-dsm/www/modules/centreon-dsm/conf.php
@@ -13,6 +13,10 @@
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
+
+
+ poutrage 
+ 
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
  *
